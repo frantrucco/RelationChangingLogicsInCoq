7f484a9dbf5a0cbe26eb3a464a94f5bc4e4947c4
beta <beta@mpi-sws.org>
2019-06-27 23:00:37 -0300

    cosmetic changes

--- src/invarianceTheorem.v
+++ src/invarianceTheorem.v
@@ -13,44 +13,44 @@ Inductive form : Type :=
 | DynDiam : Dyn -> form -> form.
 
 Definition muf : Type := forall (W : Set),
-  let R := (W -> W -> Prop) in W -> R -> (W -> R -> Prop).
+  let R := (W -> W -> Prop) in (W * R) -> ((W * R) -> Prop).
 
 Variable F : Dyn -> muf.
 
 Fixpoint satisfies
          (W : Set)
          (R : W -> W -> Prop)
-         (L : W -> prop -> Prop)
+         (V : W -> prop -> Prop)
          (w : W)
          (phi : form) : Prop :=
 match phi with
-| Atom v => (L w v)
+| Atom v => (V w v)
 | Bottom => False
-| If phi1 phi2 => (satisfies W R L w phi1) -> (satisfies W R L w phi2)
+| If phi1 phi2 => (satisfies W R V w phi1) -> (satisfies W R V w phi2)
 | Diam phi =>
-    exists v : W, R w v /\ (satisfies W R L v phi)
+    exists v : W, R w v /\ (satisfies W R V v phi)
 | DynDiam d phi =>
   let fw := F d W in
-  exists (v : W) (R' : W -> W -> Prop), fw w R v R' /\
-                             satisfies W R' L v phi
+  exists (v : W) (R' : W -> W -> Prop), fw (w, R) (v, R') /\
+                             satisfies W R' V v phi
 end.
 
-Notation "# W , R , L >> w |= phi" := (satisfies W R L w phi)
+Notation "# W , R , V >> w |= phi" := (satisfies W R V w phi)
                                       (at level 30).
 
 Definition model_to_model_relation W W' : Type :=
   W -> (W -> W -> Prop) -> W' -> (W' -> W' -> Prop) -> Prop.
 
-Definition equivalent_at_points W R L W' R' L' w w' :=
-  forall (phi:form), (# W , R , L >> w |= phi) <->
-                (# W' , R' , L' >> w' |= phi).
+Definition equivalent_at_points W R V W' R' V' w w' :=
+  forall (phi:form), (# W , R , V >> w |= phi) <->
+                (# W' , R' , V' >> w' |= phi).
 
 Definition atomic_harmony
-           (W : Set) (L : W -> prop -> Prop)
-           (W' : Set) (L' : W' -> prop -> Prop)
+           (W : Set) (V : W -> prop -> Prop)
+           (W' : Set) (V' : W' -> prop -> Prop)
            (Z : model_to_model_relation W W') : Prop :=
   forall w S w' S',
-  Z w S w' S' -> L w = L' w'.
+  Z w S w' S' -> V w = V' w'.
 
 Definition zig
            (W : Set)
@@ -73,7 +73,7 @@ Definition f_zig
            (f : muf) : Prop :=
   let (fw, fw') := (f W, f W') in
   forall w S w' S' v T, Z w S w' S' ->
-    fw w S v T -> (exists (v' : W') T', fw' w' S' v' T' /\ Z v T v' T').
+    fw (w, S) (v, T) -> (exists (v' : W') T', fw' (w', S') (v', T') /\ Z v T v' T').
 
 Definition f_zag
            (W : Set)
@@ -82,28 +82,28 @@ Definition f_zag
            (f : muf) : Prop :=
   let (fw, fw') := (f W, f W') in
   forall w S w' S' v' T', Z w S w' S' ->
-    fw' w' S' v' T' -> (exists (v : W) T, fw w S v T /\ Z v T v' T').
+    fw' (w', S') (v', T') -> (exists (v : W) T, fw (w, S) (v, T) /\ Z v T v' T').
 
-Definition bisimulation W L W' L' Z :=
-  (atomic_harmony W L W' L' Z) /\ (zig W W' Z) /\ (zag W W' Z) /\
+Definition bisimulation W V W' V' Z :=
+  (atomic_harmony W V W' V' Z) /\ (zig W W' Z) /\ (zag W W' Z) /\
   (forall d : Dyn, (f_zig W W' Z (F d))) /\
   forall d : Dyn, (f_zag W W' Z (F d)).
 
-Definition bisimulation_at_points W R L W' R' L' Z w w' :=
-  bisimulation W L W' L' Z /\ Z w R w' R'.
+Definition bisimulation_at_points W R V W' R' V' Z w w' :=
+  bisimulation W V W' V' Z /\ Z w R w' R'.
 
-Definition bisimulable W L W' L' :=
-  exists Z, bisimulation W L W' L' Z.
+Definition bisimulable W V W' V' :=
+  exists Z, bisimulation W V W' V' Z.
 
-Definition bisimulable_at_points W R L W' R' L' w w':=
-  exists Z, bisimulation W L W' L' Z /\ Z w R w' R'.
+Definition bisimulable_at_points W R V W' R' V' w w':=
+  exists Z, bisimulation W V W' V' Z /\ Z w R w' R'.
 
 Theorem InvarianceUnderBisimulation :
-  forall W R L W' R' L' w w',
-    bisimulable_at_points W R L W' R' L' w w' ->
-    equivalent_at_points W R L W' R' L' w w'.
+  forall W R V W' R' V' w w',
+    bisimulable_at_points W R V W' R' V' w w' ->
+    equivalent_at_points W R V W' R' V' w w'.
 Proof.
-  intros W R L W' R' L' w w'.
+  intros W R V W' R' V' w w'.
 
   unfold bisimulable_at_points.
   unfold equivalent_at_points.
