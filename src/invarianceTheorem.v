0b694dcd88cd23b2253834db3b44767a0d043031
Francisco Trucco <franciscoctrucco@gmail.com>
2019-10-29 22:15:49 -0300

    Improve source code.

--- src/invarianceTheorem.v
+++ src/invarianceTheorem.v
@@ -1,199 +1,158 @@
+From Coq.Relations Require Import Relations.
+
 Section invarianceTheorem.
 
+(* Syntax *)
 Variable Dyn : Set.
 
-Inductive prop: Set :=
-p : nat -> prop.
+Inductive prop : Set :=
+  p : nat -> prop.
 
 Inductive form : Type :=
-| Atom    : prop -> form
-| Bottom  : form
-| If      : form -> form -> form
-| Diam    : form -> form
-| DynDiam : Dyn -> form -> form.
+  | Atom    : prop -> form
+  | Bottom  : form
+  | If      : form -> form -> form
+  | DynDiam : Dyn -> form -> form.
+
+(* Syntactic sugar *)
+Definition Not (phi : form) : form :=
+If phi Bottom.
+
+Definition Top : form :=
+Not Bottom.
+
+Definition And (phi psi : form) : form :=
+Not (If phi (Not psi)).
+
+Definition Or (phi psi : form) : form :=
+If (Not phi) psi.
+
+Definition Iif (phi psi : form) : form :=
+And (If phi psi) (If psi phi).
+
+Definition DynBox (d : Dyn) (phi : form) : form :=
+Not (DynDiam d (Not phi)).
+
+(* Notation *)
+
+Notation "p /\' q" := (And p q)
+                     (at level 80, right associativity).
+
+Notation "p \/' q" := (Or p q)
+                     (at level 85, right associativity).
+
+Notation "~' p" := (Not p)
+                   (at level 70, right associativity).
+
+Notation "p ->' q" := (If p q)
+                     (at level 90, right associativity).
+
+Notation "p <->' q" := (Iif p q)
+                     (at level 95, right associativity).
+
+Notation "<o> d phi" := (DynDiam d phi)
+                        (at level 65, right associativity).
+
+Notation "[o] d phi" := (DynBox d phi)
+                        (at level 65, right associativity).
+
+(* Semantics *)
+
+Definition point (W: Set) : Type := (W * relation W).
 
 Definition muf : Type := forall (W : Set),
-  let R := (W -> W -> Prop) in W -> R -> (W -> R -> Prop).
+(point W) -> (point W -> Prop).
 
 Variable F : Dyn -> muf.
 
-Fixpoint satisfies
-         (W : Set)
-         (R : W -> W -> Prop)
-         (L : W -> prop -> Prop)
-         (w : W)
-         (phi : form) : Prop :=
+Fixpoint satisfies (W : Set) (R : W -> W -> Prop) (V : W -> prop -> Prop)
+                    (w : W) (phi : form) : Prop :=
 match phi with
-| Atom v => (L w v)
+| Atom a => V w a
 | Bottom => False
-| If phi1 phi2 => (satisfies W R L w phi1) -> (satisfies W R L w phi2)
-| Diam phi =>
-    exists v : W, R w v /\ (satisfies W R L v phi)
+| If phi1 phi2 => (satisfies W R V w phi1) -> (satisfies W R V w phi2)
 | DynDiam d phi =>
-  let fw := F d W in
-  exists (v : W) (R' : W -> W -> Prop), fw w R v R' /\
-                             satisfies W R' L v phi
+    let fw := F d W in
+    exists (v : W) (R' : W -> W -> Prop), fw (w, R) (v, R') /\ satisfies W R' V v phi
 end.
 
-Notation "# W , R , L >> w |= phi" := (satisfies W R L w phi)
-                                      (at level 30).
+Notation "# W , R , V >> w |= phi" := (satisfies W R V w phi) (at level 30).
 
-Definition model_to_model_relation W W' : Type :=
-  W -> (W -> W -> Prop) -> W' -> (W' -> W' -> Prop) -> Prop.
+(* Semantic Definitions *)
+Variables W W' : Set.
 
-Definition equivalent_at_points W R L W' R' L' w w' :=
-  forall (phi:form), (# W , R , L >> w |= phi) <->
-                (# W' , R' , L' >> w' |= phi).
+Variables (V : W -> prop -> Prop) (V' : W' -> prop -> Prop).
 
-Definition atomic_harmony
-           (W : Set) (L : W -> prop -> Prop)
-           (W' : Set) (L' : W' -> prop -> Prop)
-           (Z : model_to_model_relation W W') : Prop :=
-  forall w S w' S',
-  Z w S w' S' -> L w = L' w'.
+Definition equivalent_at_points '(w, R) '(w', R') :=
+forall (phi:form), (# W , R , V >> w |= phi) <-> (# W' , R' , V' >> w' |= phi).
 
-Definition zig
-           (W : Set)
-           (W' : Set)
-           (Z : model_to_model_relation W W') : Prop :=
-  forall w S w' S' v, Z w S w' S' ->
-    S w v -> (exists v' : W', Z v S v' S' /\ S' w' v').
+Definition model_to_model_relation : Type :=
+(point W) -> (point W') -> Prop.
 
-Definition zag
-           (W : Set)
-           (W' : Set)
-           (Z : model_to_model_relation W W') : Prop :=
-  forall w S w' S' v', Z w S w' S' ->
-    S' w' v' -> (exists v : W, Z v S v' S' /\ S w v).
+Variable Z : model_to_model_relation.
 
-Definition f_zig
-           (W : Set)
-           (W' : Set)
-           (Z : model_to_model_relation W W')
-           (f : muf) : Prop :=
-  let (fw, fw') := (f W, f W') in
-  forall w S w' S' v T, Z w S w' S' ->
-    fw w S v T -> (exists (v' : W') T', fw' w' S' v' T' /\ Z v T v' T').
+Definition atomic_harmony : Prop :=
+forall w S w' S', Z (w, S) (w', S') -> V w = V' w'.
 
-Definition f_zag
-           (W : Set)
-           (W' : Set)
-           (Z : model_to_model_relation W W')
-           (f : muf) : Prop :=
-  let (fw, fw') := (f W, f W') in
-  forall w S w' S' v' T', Z w S w' S' ->
-    fw' w' S' v' T' -> (exists (v : W) T, fw w S v T /\ Z v T v' T').
+Definition f_zig (f : muf) : Prop :=
+forall w S w' S' v T, Z (w, S) (w', S') ->
+    f W (w, S) (v, T) ->
+    (exists (v' : W') T', f W' (w', S') (v', T') /\ Z (v, T) (v', T')).
 
-Definition bisimulation W L W' L' Z :=
-  (atomic_harmony W L W' L' Z) /\ (zig W W' Z) /\ (zag W W' Z) /\
-  (forall d : Dyn, (f_zig W W' Z (F d))) /\
-  forall d : Dyn, (f_zag W W' Z (F d)).
+Definition f_zag (f : muf) : Prop :=
+forall w S w' S' v' T', Z (w, S) (w', S') ->
+    f W' (w', S') (v', T') ->
+    (exists (v : W) T, f W (w, S) (v, T) /\ Z (v, T) (v', T')).
 
-Definition bisimulation_at_points W R L W' R' L' Z w w' :=
-  bisimulation W L W' L' Z /\ Z w R w' R'.
+Definition bisimulation : Prop := atomic_harmony /\
+(forall d : Dyn, (f_zig (F d))) /\ (forall d : Dyn, (f_zag (F d))).
 
-Definition bisimulable W L W' L' :=
-  exists Z, bisimulation W L W' L' Z.
+Definition bis_at_points (p: point W) (p': point W') : Prop :=
+bisimulation /\ Z p p'.
 
-Definition bisimulable_at_points W R L W' R' L' w w':=
-  exists Z, bisimulation W L W' L' Z /\ Z w R w' R'.
+(* Theorem *)
 
 Theorem InvarianceUnderBisimulation :
-  forall W R L W' R' L' w w',
-    bisimulable_at_points W R L W' R' L' w w' ->
-    equivalent_at_points W R L W' R' L' w w'.
-Proof.
-  intros W R L W' R' L' w w'.
+forall (p: point W) (p': point W'),
+    bis_at_points p p' -> equivalent_at_points p p'.
 
-  unfold bisimulable_at_points.
+Proof.
+  intros [w S] [w' S'].           (* name each component of the points *)
+  unfold bis_at_points.                          (* unfold definitions *)
   unfold equivalent_at_points.
   unfold bisimulation.
 
-  intros [Z [[HAtomicHarmony [HZig [HZag [HFZig HFZag]]]] HZwSw'S']].
+  intros [[HAtomicHarmony [HFZig HFZag]] HZwSw'S'].
   intros phi.
+  generalize dependent S'. generalize dependent S.
+  generalize dependent w'. generalize dependent w.
 
-  generalize dependent Z.
-  generalize dependent R'.
-  generalize dependent R.
-  generalize dependent w'.
-  generalize dependent w.
-
-  induction phi as [p | | phi IHphi psi IHpsi | phi IH | d phi IH];
-    unfold satisfies; fold satisfies; 
-    intros w w' S S' Z HAtomicHarmony HZig HZag HFZig HFZag HZwSw'S'.
-
-  + (* Atom *)
-    rewrite (HAtomicHarmony w S w' S' HZwSw'S').
-    tauto.
-
-  + (* Bottom *)
-    tauto.
-
-  + (* If *)
-    split;
-      intros H Hsat;
-      apply (IHpsi w w' S S' Z
-                   HAtomicHarmony HZig HZag HFZig HFZag HZwSw'S');
-      apply H;
-      apply (IHphi w w' S S' Z
-                   HAtomicHarmony HZig HZag HFZig HFZag HZwSw'S');
-      apply Hsat.
-
-  + (* Modal *)
-    split.
-    - intro H.
-      destruct H as [v [HSwv Hsatv]].
-      unfold zig in HZig.
-      apply (HZig w S w' S' v HZwSw'S') in HSwv as [v' [HZvv' HS'w'v']].
-      exists v'.
-      split.
-      * assumption.
-      * apply (IH v v' S S' Z
-                  HAtomicHarmony HZig HZag HFZig HFZag HZvv').
-        assumption.
-    - intro H.
-      unfold zag in HZag.
-      destruct H as [v' H].
-      destruct H as [HSw'v' Hsatv'].
-      apply (HZag w S w' S' v' HZwSw'S') in HSw'v' as Hexists.
-      destruct Hexists as [v Hexists].
-      destruct Hexists as [HZvv' HSwv].
-      exists v.
-      split.
-      * assumption.
-      * apply (IH v v' S S' Z
-                  HAtomicHarmony HZig HZag HFZig HFZag HZvv').
-        assumption.
-
-  + (* Dynamic *)
-    split.
-    - intro H.
-      destruct H as [v [T [HfWwSvT HsatTv]]].
-
+  induction phi as [p | | phi IHphi psi IHpsi | d phi IH].
+  simpl;                (* This tactic unfolds definitions *)
+  intros w w' S S' HZwSw'S'.
+  + rewrite (HAtomicHarmony w S w' S' HZwSw'S'). tauto.
+  + tauto.
+  + split;
+    intros HIf Hsat;
+    apply (IHpsi w w' S S' HZwSw'S');
+    apply HIf;
+    apply (IHphi w w' S S' HZwSw'S');
+    apply Hsat.
+  + split. simpl.
+    - intros [v [T [HfWwSvT HsatTv]]].
       apply (HFZig d w S w' S' v T HZwSw'S') in HfWwSvT
-        as [v' [T' [HfW'w'S'v'T' HZvTv'T']]].
-
+          as [v' [T' [HfW'w'S'v'T' HZvTv'T']]].
       exists v'. exists T'.
-
       split.
-        * assumption.
-        * apply (IH v v' T T' Z HAtomicHarmony HZig
-                    HZag HFZig HFZag HZvTv'T').
-          assumption.
-
-
-    - intro H.
-      destruct H as [v' [T' [HfW'w'S'v'T' HsatT'v']]].
-
-      apply (HFZag d w S w' S' v' T' HZwSw'S') in HfW'w'S'v'T'
-        as [v [T [HfWwSvT HZvTv'T']]].
-
-      exists v. exists T.
-
+      * assumption.
+      * eapply IH. apply HZvTv'T'. assumption.
+    - intros [v [T [HfWwSvT HsatTv]]].
+      apply (HFZag d w S w' S' v T HZwSw'S') in HfWwSvT
+          as [v' [T' [HfW'w'S'v'T' HZvTv'T']]].
+      exists v'. exists T'.
       split.
-        * assumption.
-        * apply (IH v v' T T' Z HAtomicHarmony HZig
-                    HZag HFZig HFZag HZvTv'T').
-          assumption.
+      * assumption.
+      * eapply IH. apply HZvTv'T'. assumption.
 Qed.
+
 End invarianceTheorem.
