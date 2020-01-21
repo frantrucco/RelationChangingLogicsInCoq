e1faffca9e925eec1cf6a905c30a0d93b09f26b2
beta <beta@mpi-sws.org>
2020-01-20 21:41:46 -0300

    moving to use point instead of triples for everything (simpler code, although further away from text). generalizing to valuation changing operators.

--- src/invarianceTheorem.v
+++ src/invarianceTheorem.v
@@ -1,3 +1,4 @@
+Require Import ssreflect.
 From Coq.Relations Require Import Relations.
 
 Section invarianceTheorem.
@@ -16,22 +17,22 @@ Inductive form : Type :=
 
 (* Syntactic sugar *)
 Definition Not (phi : form) : form :=
-If phi Bottom.
+  If phi Bottom.
 
 Definition Top : form :=
-Not Bottom.
+  Not Bottom.
 
 Definition And (phi psi : form) : form :=
-Not (If phi (Not psi)).
+  Not (If phi (Not psi)).
 
 Definition Or (phi psi : form) : form :=
-If (Not phi) psi.
+  If (Not phi) psi.
 
 Definition Iif (phi psi : form) : form :=
-And (If phi psi) (If psi phi).
+  And (If phi psi) (If psi phi).
 
 Definition DynBox (d : Dyn) (phi : form) : form :=
-Not (DynDiam d (Not phi)).
+  Not (DynDiam d (Not phi)).
 
 (* Notation *)
 
@@ -57,58 +58,64 @@ Notation "[o] d phi" := (DynBox d phi)
                         (at level 65, right associativity).
 
 (* Semantics *)
+Definition val (W: Set) : Type := W -> prop -> Prop.
 
-Definition point (W: Set) : Type := (W * relation W).
+Definition point (W: Set) : Type := (W * relation W * val W).
+
+Definition value {W} (p: point W) :=
+  let '(v, _, _) := p in v.
+Definition rel {W} (p: point W) :=
+  let '(_, r, _) := p in r.
+Definition valuation {W} (p: point W) :=
+  let '(_, _, v) := p in v.
 
 Definition muf : Type := forall (W : Set),
-(point W) -> (point W -> Prop).
+  point W -> (point W -> Prop).
 
 Variable F : Dyn -> muf.
 
-Fixpoint satisfies (W : Set) (R : W -> W -> Prop) (V : W -> prop -> Prop)
-                    (w : W) (phi : form) : Prop :=
-match phi with
-| Atom a => V w a
-| Bottom => False
-| If phi1 phi2 => (satisfies W R V w phi1) -> (satisfies W R V w phi2)
-| DynDiam d phi =>
+Fixpoint satisfies (W : Set) (p: point W) (phi : form) : Prop :=
+  match phi with
+  | Atom a => valuation p (value p) a
+  | Bottom => False
+  | If phi1 phi2 => (satisfies W p phi1) -> (satisfies W p phi2)
+  | DynDiam d phi =>
     let fw := F d W in
-    exists (v : W) (R' : W -> W -> Prop), fw (w, R) (v, R') /\ satisfies W R' V v phi
-end.
+    exists p', fw p p' /\ satisfies W p' phi
+  end.
 
-Notation "# W , R , V >> w |= phi" := (satisfies W R V w phi) (at level 30).
+Notation "<< W , p >> |= phi" := (satisfies W p phi) (at level 30).
 
 (* Semantic Definitions *)
 Variables W W' : Set.
 
-Variables (V : W -> prop -> Prop) (V' : W' -> prop -> Prop).
-
-Definition equivalent_at_points '(w, R) '(w', R') :=
-forall (phi:form), (# W , R , V >> w |= phi) <-> (# W' , R' , V' >> w' |= phi).
+Definition equivalent_at_points p p' :=
+  forall (phi:form), (<<W , p>> |= phi) <-> (<<W' , p' >> |= phi).
 
 Definition model_to_model_relation : Type :=
-(point W) -> (point W') -> Prop.
+  point W -> point W' -> Prop.
 
 Variable Z : model_to_model_relation.
 
 Definition atomic_harmony : Prop :=
-forall w S w' S', Z (w, S) (w', S') -> V w = V' w'.
+  forall p p', Z p p' -> valuation p (value p) = valuation p' (value p').
 
 Definition f_zig (f : muf) : Prop :=
-forall w S w' S' v T, Z (w, S) (w', S') ->
-    f W (w, S) (v, T) ->
-    (exists (v' : W') T', f W' (w', S') (v', T') /\ Z (v, T) (v', T')).
+  forall p q p', Z p p' ->
+    f W p q ->
+    (exists q', f W' p' q' /\ Z q q').
 
 Definition f_zag (f : muf) : Prop :=
-forall w S w' S' v' T', Z (w, S) (w', S') ->
-    f W' (w', S') (v', T') ->
-    (exists (v : W) T, f W (w, S) (v, T) /\ Z (v, T) (v', T')).
+  forall p q' p', Z p p' ->
+    f W' p' q' ->
+    (exists q, f W p q /\ Z q q').
 
-Definition bisimulation : Prop := atomic_harmony /\
-(forall d : Dyn, (f_zig (F d))) /\ (forall d : Dyn, (f_zag (F d))).
+Definition bisimulation : Prop :=
+  atomic_harmony /\
+  (forall d : Dyn, (f_zig (F d))) /\ (forall d : Dyn, (f_zag (F d))).
 
 Definition bis_at_points (p: point W) (p': point W') : Prop :=
-bisimulation /\ Z p p'.
+  bisimulation /\ Z p p'.
 
 (* Theorem *)
 
@@ -117,42 +124,42 @@ forall (p: point W) (p': point W'),
     bis_at_points p p' -> equivalent_at_points p p'.
 
 Proof.
-  intros [w S] [w' S'].           (* name each component of the points *)
-  unfold bis_at_points.                          (* unfold definitions *)
+  intros p p'. (* name each component of the points *)
+  unfold bis_at_points.          (* unfold definitions *)
   unfold equivalent_at_points.
   unfold bisimulation.
 
   intros [[HAtomicHarmony [HFZig HFZag]] HZwSw'S'].
   intros phi.
-  generalize dependent S'. generalize dependent S.
-  generalize dependent w'. generalize dependent w.
-
-  induction phi as [p | | phi IHphi psi IHpsi | d phi IH];
-  simpl;                (* This tactic unfolds definitions *)
-  intros w w' S S' HZwSw'S'.
-  + rewrite (HAtomicHarmony w S w' S' HZwSw'S'). tauto.
+  
+  generalize dependent p'. generalize dependent p.
+  
+  induction phi as [prop | | phi IHphi psi IHpsi | d phi IH];
+  simpl.                (* This tactic unfolds definitions *)
+  + intros p p' HZpp'. rewrite (HAtomicHarmony _ _ HZpp'). tauto.
   + tauto.
-  + split;
+  + intros p p'; split;
     intros HIf Hsat;
-    apply (IHpsi w w' S S' HZwSw'S');
+    apply (IHpsi _ _ HZwSw'S');
     apply HIf;
-    apply (IHphi w w' S S' HZwSw'S');
+    apply (IHphi _ _ HZwSw'S');
     apply Hsat.
-  + split. simpl.
-    - intros [v [T [HfWwSvT HsatTv]]].
-      apply (HFZig d w S w' S' v T HZwSw'S') in HfWwSvT
-          as [v' [T' [HfW'w'S'v'T' HZvTv'T']]].
-      exists v'. exists T'.
+  + intros p p'. split; simpl.
+    - intros [q [HfWpp' Hsatq]].
+      apply (HFZig _ _ _ _ HZwSw'S') in HfWpp'
+          as [q' [HfW'q'p' HZqq']].
+      eexists.
       split.
-      * assumption.
-      * eapply IH. apply HZvTv'T'. assumption.
-    - intros [v [T [HfWwSvT HsatTv]]].
-      apply (HFZag d w S w' S' v T HZwSw'S') in HfWwSvT
-          as [v' [T' [HfW'w'S'v'T' HZvTv'T']]].
-      exists v'. exists T'.
+      * eassumption.
+      * eapply IH. eassumption.
+        assumption.
+    - intros [q' [HfWpp' Hsatq']].
+      apply (HFZag _ _ _ _ HZwSw'S') in HfWpp'
+          as [q [HfWpq HZqq']].
+      eexists.
       split.
-      * assumption.
-      * eapply IH. apply HZvTv'T'. assumption.
+      * eassumption.
+      * eapply IH. eassumption. assumption.
 Qed.
 
 End invarianceTheorem.
