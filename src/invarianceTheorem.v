6d3bfbb4beb44f426b077a205f48e777c52ea1b8
beta <beta@mpi-sws.org>
2020-01-23 18:52:28 -0300

    WIP

--- src/invarianceTheorem.v
+++ src/invarianceTheorem.v
@@ -2,6 +2,8 @@ From Mtac2 Require Import Mtac2.
 From Coq.Relations Require Import Relations.
 From Coq.Lists Require Import List.
 
+Require Import ssreflect.
+
 Module Utilities. (* move to file! *)
 
 Obligation Tactic := idtac.
@@ -69,7 +71,18 @@ Arguments st_point {W}.
 Arguments st_rel {W}.
 Arguments st_val {W}.
 
+Definition to_pm {W} (sm: state_model W) :=
+  {| pm_model := {| m_rel := sm.(st_rel); m_val := sm.(st_val) |};
+     pm_point := sm.(st_point) |}.
 
+Coercion to_pm: state_model >-> pointed_model.
+
+Definition to_st (pm: pointed_model) :=
+  {| st_rel := pm.(m_rel);
+     st_val := pm.(m_val);
+     st_point := pm.(pm_point) |}.
+
+Coercion to_st: pointed_model >-> state_model.
 
 Section InvarianceTheorem.
 
@@ -132,31 +145,35 @@ Definition muf : Type := forall (W : Set),
 
 Variable F : Dyn -> muf.
 
-Fixpoint satisfies {W : Set} (p: state_model W) (phi : form) : Prop :=
+Fixpoint satisfies (pm: pointed_model) (phi : form) : Prop :=
   match phi with
-  | Atom a => p.(st_val) p.(st_point) a
+  | Atom a => pm.(m_val) pm.(pm_point) a
   | Bottom => False
-  | If phi1 phi2 => (satisfies p phi1) -> (satisfies p phi2)
+  | If phi1 phi2 => (satisfies pm phi1) -> (satisfies pm phi2)
   | DynDiam d phi =>
-    let fw := F d W in
-    exists p', fw p p' /\ satisfies p' phi
+    let fw := F d pm in
+    exists p', fw pm p' /\ satisfies p' phi
   end.
 
 Notation "p |= phi" := (satisfies p phi) (at level 30).
 
+Definition equivalent (_M _M': pointed_model) :=
+  forall (ϕ: form), (_M |= ϕ) <-> (_M' |= ϕ).
+
+Notation "m ≡ m'" := (equivalent m m') (at level 0).
+
 (* Semantic Definitions *)
+Section Bisimulation.
+
 Variables W W' : Set.
 
-Definition equivalent_at_sm (p: state_model W) (p': state_model W') :=
-  forall (ϕ: form), (p |= ϕ) <-> (p' |= ϕ).
-
-Definition model_to_model_relation : Type :=
+Definition state_model_relation : Type :=
   state_model W -> state_model W' -> Prop.
 
-Variable Z : model_to_model_relation.
+Variable Z : state_model_relation.
 
 Definition atomic_harmony : Prop :=
-  forall p p', Z p p' -> p.(st_val) p.(st_point) = p'.(st_val) p'.(st_point).
+  forall p p', Z p p' -> forall pr: prop, p.(st_val) p.(st_point) pr = p'.(st_val) p'.(st_point) pr.
 
 Definition f_zig (f : muf) : Prop :=
   forall p q p', Z p p' ->
@@ -172,45 +189,90 @@ Definition bisimulation : Prop :=
   atomic_harmony /\
   (forall d : Dyn, (f_zig (F d))) /\ (forall d : Dyn, (f_zag (F d))).
 
-Definition bis_at_sm (p: state_model W) (p': state_model W') : Prop :=
-  bisimulation /\ Z p p'.
+End Bisimulation.
 
-(* Theorem *)
+Arguments bisimulation {_ _}.
 
+Definition bisimilar (_M _M': pointed_model) : Prop :=
+  exists Z, bisimulation Z /\ Z _M _M'.
+
+(* Main Theorem *)
+
+Lemma to_st_val (_M: pointed_model) : m_val _M = st_val _M.
+  by [].
+Qed.
+
+Lemma to_st_point (_M: pointed_model) : pm_point _M = st_point _M.
+  by [].
+Qed.
+
+(* Lemma to_pm_sat (_M: pointed_model) (st: state_model _M) (ϕ: form) : *)
+(*   _M |= ϕ <-> st |= ϕ. *)
+(*   destruct _M. *)
+(*   destruct st. *)
+(*   destruct pm_model0. *)
+(*   simpl in *. *)
+(*   by []. *)
+(* Qed. *)
+
+(* Definition get_HA {W W'} {Z: state_model_relation W W'} (bis: bisimulation Z) : atomic_harmony _ _ Z. *)
+(*   move: bis =>[HA _]. *)
+(*   exact: HA. *)
+(* Defined. *)
+
+(* Definition get_Zig {W W'} {Z: state_model_relation W W'} (bis: bisimulation Z) : (forall d : Dyn, (f_zig ?? Z (F d))). *)
+(*   move: bis =>[_ [H _]]. *)
+(*   exact: H. *)
+(* Defined. *)
+
+(* Definition get_Zag {W W'} {Z: state_model_relation W W'} (bis: bisimulation Z) : (forall d : Dyn, (f_zag ?? Z (F d))). *)
+(*   move: bis =>[_ [_ H]]. *)
+(*   exact: H. *)
+(* Defined. *)
+
+(* Theorem bisimilar_sym _M _M': bisimilar _M _M' <-> bisimilar _M' _M. *)
+(* Proof. *)
+(* Admitted.   *)
 
 Theorem InvarianceUnderBisimulation :
-forall (p: state_model W) (p': state_model W'),
-    bis_at_sm p p' -> equivalent_at_sm p p'.
+  forall _M _M' : pointed_model,
+  bisimilar _M _M' -> _M ≡ _M'.
 
 Proof.
-  intros p p'. (* name each component of the points *)
-  unfold bis_at_sm.          (* unfold definitions *)
-  unfold equivalent_at_sm.
-  unfold bisimulation.
-
-  intros [ [HAtomicHarmony [HFZig HFZag]] HZwSw'S'].
-  intros ϕ.
-
-  generalize dependent p'. generalize dependent p.
-
-  induction ϕ as [prop | | ϕ IHϕ ψ IHψ | d ϕ IH];
-  simpl.                (* This tactic unfolds definitions *)
-  + intros p p' HZpp'. rewrite (HAtomicHarmony ?? HZpp'). tauto.
+  move=> _M _M' bis ϕ.
+  move: _M _M' bis.
+  (* intros [Z [ [HAtomicHarmony [HFZig HFZag]] HZ]]. *)
+  (* intros ϕ. *)
+  induction ϕ as [prop | | ϕ IHϕ ψ IHψ | d ϕ IH]; simpl;
+  intros _M _M' [Z [bis HZ]].
+  + rewrite !to_st_val !to_st_point ((get_HA bis) ?? HZ).
+    tauto.
   + tauto.
-  + intros p p'; split;
-    intros HIf Hsat;
-    apply (IHψ ?? HZwSw'S');
-    apply HIf;
-    apply (IHϕ ?? HZwSw'S');
-    apply Hsat.
-  + intros p p'. split; simpl.
+  + split;
+    intros HIf Hsat.
+    eapply (IHψ _M).
+    unfold bisimilar. eexists. split; eassumption.
+    apply HIf.
+    eapply (IHϕ _M).
+    unfold bisimilar. eexists. split; eassumption.
+    eassumption.
+
+    eapply (IHψ _M).
+    unfold bisimilar. eexists. split; eassumption.
+    apply HIf.
+    eapply (IHϕ _M).
+    unfold bisimilar. eexists. split; eassumption.
+    eassumption.
+    
+  + split; simpl.
     - intros [q [HfWpp' Hsatq]].
-      apply (HFZig ?? HZwSw'S') in HfWpp'
+      eapply (get_Zig bis) in HfWpp'
           as [q' [HfW'q'p' HZqq']].
       eexists.
       split.
       * eassumption.
-      * eapply IH; eassumption.
+      * Set Printing Coercions. 
+        eapply IH; eassumption.
     - intros [q' [HfWpp' Hsatq']].
       apply (HFZag ?? HZwSw'S') in HfWpp'
           as [q [HfWpq HZqq']].
